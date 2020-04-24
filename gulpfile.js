6b780f76684fba3f83b4509766a0e41aa809a580
Jero Soler <jero.soler@gmail.com>
2020-04-24 13:23:35 +0200

    Gulp minifi

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
+
+
+
+
+gulp.task('js', done => {
+   return gulp.src('src/*.js')
+  .pipe(minify())
+  .pipe(concat('drawflow.min.js'))
+  .pipe(gulp.dest('dist/'))
+    done();
+});
+
+
+
+gulp.task('css', done => {
+  return gulp.src('src/*.css')
+  .pipe(minifyCSS())
+  .pipe(concat('drawflow.min.css'))
+  .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('default', gulp.parallel(
+        'js',
+        'css'
+  )
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/',
   output: {
-    filename: 'main.js',
+    filename: 'drawflow.min.js',
     libraryTarget: 'umd',
     library: 'Drawflow',
     umdNamedDefine: true,
 	  libraryExport: 'default'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "drawflow.min.css",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
   }
 };
