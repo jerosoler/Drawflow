fe3884c044c92e6e5e5182f9d37756da105290a9
Jero Soler <jero.soler@gmail.com>
2020-07-11 23:12:57 +0200

    Fix remove node in other module page

--- src/drawflow.js
+++ src/drawflow.js
@@ -794,8 +794,11 @@ export default class Drawflow {
   }
 
   removeNodeId(id) {
-    document.getElementById(id).remove();
-    delete this.drawflow.drawflow[this.module].data[id.slice(5)];
+    var moduleName = this.getModuleFromNodeId(id.slice(5))
+    if(this.module === moduleName) {
+      document.getElementById(id).remove();
+    }
+    delete this.drawflow.drawflow[moduleName].data[id.slice(5)];
     this.dispatch('nodeRemoved', id.slice(5));
     this.removeConnectionNodeId(id);
   }
