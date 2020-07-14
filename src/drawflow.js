0064c26e929570b31aa4bafe2a948ecb7a72c86a
Jero Soler <jero.soler@gmail.com>
2020-07-14 16:44:36 +0200

    Fix contenteditable delete

--- src/drawflow.js
+++ src/drawflow.js
@@ -415,7 +415,7 @@ export default class Drawflow {
     }
     if (e.key === 'Delete' || (e.key === 'Backspace' && e.metaKey)) {
       if(this.node_selected != null) {
-        if(this.first_click.tagName !== 'INPUT' && this.first_click.tagName !== 'TEXTAREA') {
+        if(this.first_click.tagName !== 'INPUT' && this.first_click.tagName !== 'TEXTAREA' && this.first_click.hasAttribute('contenteditable') !== true) {
           this.removeNodeId(this.node_selected.id);
         }
       }
